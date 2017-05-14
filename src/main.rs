60563cba53b44fff200bbc8b6c5f2743c390ab67
slme9364 <ishikazu1104@yahoo.co.jp>
2017-05-14 11:34:47 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate rand;
+
+use rand::Rng;
+
+fn make_sound() -> String {
+    let sounds = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+    let mut rng = rand::thread_rng();
+    let random_index = rng.gen::<usize>() % sounds.len();
+    let sound_string = sounds[random_index].to_string();
+    sound_string
+}
+
+fn main() {
+    for i in 0..8 {
+        print!("{} ", make_sound());
+    }
+    println!("");
+}
