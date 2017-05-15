5e2bea6b2d6bb74adf417b8124fc3999bc1c956f
slme9364 <ishikazu1104@yahoo.co.jp>
2017-05-15 14:21:34 +0900

    [Add] add sound

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 use rand::Rng;
 
 fn make_sound() -> String {
-    let sounds = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+    let sounds = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "休"];
     let mut rng = rand::thread_rng();
     let random_index = rng.gen::<usize>() % sounds.len();
     let sound_string = sounds[random_index].to_string();
